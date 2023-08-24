e1d69a756eda35c9901bd5d40ed4367016ccf91c
Margarita Slesareva <slesareva.m.a@yandex.ru>
2023-08-24 21:57:46 +0300

    delete soryboard

---
+++ Advertisements/Supporting files/SceneDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  SceneDelegate.swift
+//  Advertisements
+//
+//  Created by Margarita Slesareva on 24.08.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
