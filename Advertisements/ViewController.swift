fc689fb9b756f9f00d0b33789409a6f69bf7935a
Margarita Slesareva <slesareva.m.a@yandex.ru>
2023-08-24 21:44:12 +0300

    Initial Commit

---
+++ Advertisements/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Advertisements
+//
+//  Created by Margarita Slesareva on 24.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
